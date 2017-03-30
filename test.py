aa5d7f023c022336ace0a8cbf50310fae90dd799
jimbray <jimbray16@gmail.com>
2017-03-30 17:46:47 +0800

    add file

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
+# -*- coding: utf-8 -*-
 
+__author__ = 'jimbray'
 
-def getText():
-	return "This is the test text."
+import os,datetime
+
+def get_release_time():
+    today = datetime.datetime.now()
+    return today.strftime('%Y-%m-%d-%H-%M-%S')
+
+def copy_apk_to_release_share_folder():
+    dir_name = 'C:\\Users\\dell\\.jenkins\\workspace\\Poof-Android\\app\\build\\outputs\\apk\\'
+    target_dir_name = 'H:\\Poof_for_Android_Release\\'
+    for file in os.listdir(dir_name):
+        source_file_full_name = os.path.join(dir_name, file)
+        target_file_full_name = os.path.join(target_dir_name, file)
+        target_file_full_name = target_file_full_name.replace('Poof-', 'Poof-' + get_release_time() + '-')
+        if os.path.isfile(source_file_full_name):
+            os.system('copy %s %s' % (source_file_full_name, target_file_full_name))
+
 
 if __name__ == '__main__':
-	print (getText())
+    copy_apk_to_release_share_folder()
--- yg-play.py
+++ yg-play.py
@@ -1,1 +1,25 @@
-__author__ = 'dell'
+# -*- coding=utf-8 -*-
+
+__author__ = 'jimbray'
+
+import os
+import sys
+
+
+def play_with_vlc(url):
+    # os.system('C:\n')
+    # os.system('cd \\\n')
+    # os.system('cd C:\\Program Files (x86)\\VideoLAN\\VLC\n')
+    os.chdir('C:\\Program Files (x86)\\VideoLAN\\VLC')
+    os.system('you-get -p vlc.exe ' + url)
+
+if __name__=='__main__':
+    # 第一个参数 是 文件路径
+    if len(sys.argv) > 1:
+        # 至少有一个参数
+        url = sys.argv[1]
+    else:
+        print('请在后面加入播放地址')
+
+    if url is not None:
+        play_with_vlc(url)
